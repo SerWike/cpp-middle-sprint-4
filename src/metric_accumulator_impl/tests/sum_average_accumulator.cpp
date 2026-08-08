cfa5edf561124bbe9c60a47b649ec8eb2082ef37
SerWike <fullname123@mail.ru>
2026-08-08 10:17:04 +0000

    Release tests for accumulators

--- src/metric_accumulator_impl/tests/sum_average_accumulator.cpp
+++ src/metric_accumulator_impl/tests/sum_average_accumulator.cpp
@@ -6,6 +6,68 @@
 
 namespace analyzer::metric_accumulator::metric_accumulator_impl::test {
 
-// здесь ваш код
+TEST(SumAverageAccumulatorTest, ValidTest) {
+    std::string metric_name = "Code lines count";
+    std::vector<int> values = {11, 20, 30, 40, 50};
+    SumAverageAccumulator accumulator;
+
+    std::ranges::for_each(values, [&accumulator, &metric_name](int &val) {
+        accumulator.Accumulate(metric::MetricResult{metric_name, val});
+    });
+
+    accumulator.Finalize();
+
+    ASSERT_EQ(SumAverageAccumulator::SumAverage(151, 30.2), accumulator.Get());
+}
+
+TEST(SumAverageAccumulatorTest, EmptyTest) {
+    std::string metric_name = "Code lines count";
+    std::vector<int> values = {};
+    SumAverageAccumulator accumulator;
+
+    std::ranges::for_each(values, [&accumulator, &metric_name](int &val) {
+        accumulator.Accumulate(metric::MetricResult{metric_name, val});
+    });
+
+    accumulator.Finalize();
+
+    ASSERT_EQ(SumAverageAccumulator::SumAverage(0, 0.0), accumulator.Get());
+}
+
+TEST(SumAverageAccumulatorTest, InvalidGetTest) {
+    std::string metric_name = "Code lines count";
+    SumAverageAccumulator accumulator;
+
+    accumulator.Accumulate(metric::MetricResult{metric_name, 100});
+    ASSERT_THROW(accumulator.Get(), std::runtime_error);
+}
+
+TEST(SumAverageAccumulatorTest, ResetTest) {
+    std::string metric_name = "Code lines count";
+    std::vector<int> values = {11, 20, 30, 40, 50};
+    SumAverageAccumulator accumulator;
+
+    std::ranges::for_each(values, [&accumulator, &metric_name](int &val) {
+        accumulator.Accumulate(metric::MetricResult{metric_name, val});
+    });
+
+    accumulator.Reset();
+
+    ASSERT_THROW(accumulator.Get(), std::runtime_error);
+}
+
+TEST(SumAverageAccumulatorTest, ResetAfterFinalizeTest) {
+    std::string metric_name = "Code lines count";
+    SumAverageAccumulator accumulator;
+
+    accumulator.Accumulate(metric::MetricResult{metric_name, 100});
+    accumulator.Finalize();
+    ASSERT_EQ(SumAverageAccumulator::SumAverage(100, 100.0), accumulator.Get());
+
+    accumulator.Reset();
+    accumulator.Accumulate(metric::MetricResult{metric_name, 200});
+    accumulator.Finalize();
+    ASSERT_EQ(SumAverageAccumulator::SumAverage(200, 200.0), accumulator.Get());
+}
 
 }  // namespace analyzer::metric_accumulator::metric_accumulator_impl::test
